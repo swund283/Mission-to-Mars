f17c4e0161326ffcc0259344d9e3dc3ac875baad
sam wunderilch <swund@MacBook-Pro.home>
2020-07-27 08:51:44 -0400

    challenge

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 import scraping
 
 app = Flask(__name__)
+app.debug = True
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
